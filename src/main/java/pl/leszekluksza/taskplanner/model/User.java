41af6df99b1dc2973eb0c67e1041803aec54da51
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-15 15:45:43 +0200

    Add user model

---
+++ src/main/java/pl/leszekluksza/taskplanner/model/User.java
@@ -0,0 +1,42 @@
+package pl.leszekluksza.taskplanner.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Task> tasks = new HashSet<>();
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
