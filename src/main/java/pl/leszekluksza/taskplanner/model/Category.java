5ca9c38aa22a8df2b67d8d18d3b26f87a73e3888
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-15 15:45:13 +0200

    Add category model

---
+++ src/main/java/pl/leszekluksza/taskplanner/model/Category.java
@@ -0,0 +1,45 @@
+package pl.leszekluksza.taskplanner.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Task> tasks = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
