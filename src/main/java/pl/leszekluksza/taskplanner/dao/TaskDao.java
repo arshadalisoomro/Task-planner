bcb132dc7ca010c02348fa8c10c8762afb3d3607
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-15 16:28:28 +0200

    Add taskdao with add and finish method

---
+++ src/main/java/pl/leszekluksza/taskplanner/dao/TaskDao.java
@@ -0,0 +1,34 @@
+package pl.leszekluksza.taskplanner.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import pl.leszekluksza.taskplanner.model.Task;
+import pl.leszekluksza.taskplanner.repository.TaskRepository;
+
+import java.util.Optional;
+
+@Component
+public class TaskDao {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    public String add(Task task) {
+        taskRepository.save(new Task());
+        return "saved";
+    }
+
+    public String finish(String taskId) {
+        Optional<Task> task = taskRepository.findById(Long.valueOf(taskId));
+        if(task.isPresent())
+        {
+            Task taskToFinish = task.get();
+            taskToFinish.setEnabled(false);
+            taskRepository.save(taskToFinish);
+            return "Task has been successfully finished";
+        }
+        else{
+            return "Task does not exist";
+        }
+    }
+}
