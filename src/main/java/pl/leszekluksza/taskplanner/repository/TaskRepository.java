cdcc61452e8351d558c8e29985753ac6a90dde77
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-15 16:27:32 +0200

    Add findbyid

--- src/main/java/pl/leszekluksza/taskplanner/repository/TaskRepository.java
+++ src/main/java/pl/leszekluksza/taskplanner/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package pl.leszekluksza.taskplanner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.leszekluksza.taskplanner.model.Task;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findById(Long id);
 }
