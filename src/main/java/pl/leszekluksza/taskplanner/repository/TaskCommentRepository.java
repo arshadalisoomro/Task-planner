3d33b3eae9b7135967c5bec07e38948fdeb3d279
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-17 15:59:08 +0200

    Fixed bug in inerface

--- src/main/java/pl/leszekluksza/taskplanner/repository/TaskCommentRepository.java
+++ src/main/java/pl/leszekluksza/taskplanner/repository/TaskCommentRepository.java
@@ -2,6 +2,7 @@ package pl.leszekluksza.taskplanner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.leszekluksza.taskplanner.model.Task;
+import pl.leszekluksza.taskplanner.model.TaskComment;
 
-public interface TaskCommentRepository extends JpaRepository<Task, Long> {
+public interface TaskCommentRepository extends JpaRepository<TaskComment, Long> {
 }
