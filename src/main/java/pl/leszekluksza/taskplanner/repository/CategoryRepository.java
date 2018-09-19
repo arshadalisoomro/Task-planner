9ca11bcaed4bb3c1aab9d9441bc6584be83a947a
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 18:04:37 +0200

    Add findByUserId

--- src/main/java/pl/leszekluksza/taskplanner/repository/CategoryRepository.java
+++ src/main/java/pl/leszekluksza/taskplanner/repository/CategoryRepository.java
@@ -3,5 +3,11 @@ package pl.leszekluksza.taskplanner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.leszekluksza.taskplanner.model.Category;
 
+import java.util.Set;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Set<Category> findByUserId(Long userId);
+
+    Category findByUserIdAndName(Long id, String name);
 }
