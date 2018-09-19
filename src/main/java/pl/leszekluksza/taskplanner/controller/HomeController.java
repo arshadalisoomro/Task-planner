4d5b48cd93190dd848327b47073dc52a08a3ea75
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 16:00:02 +0200

    Add login and register

--- src/main/java/pl/leszekluksza/taskplanner/controller/HomeController.java
+++ src/main/java/pl/leszekluksza/taskplanner/controller/HomeController.java
@@ -1,6 +1,7 @@
 package pl.leszekluksza.taskplanner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.leszekluksza.taskplanner.converter.FullTaskDtoConverter;
 import pl.leszekluksza.taskplanner.dto.FullTaskDto;
+import pl.leszekluksza.taskplanner.model.Task;
+import pl.leszekluksza.taskplanner.model.User;
+import pl.leszekluksza.taskplanner.repository.TaskRepository;
+import pl.leszekluksza.taskplanner.repository.UserRepository;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -17,10 +23,21 @@ public class HomeController {
     @Autowired
     FullTaskDtoConverter fullTaskDtoConverter;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TaskRepository taskRepository;
 
     @GetMapping("index")
-    public String index() {
+    public String index(Model model, Principal principal) {
+//        List<Task> tasks = taskRepository.findAll();
+        User user = userRepository.findByUsername(principal.getName());
+        List<Task> tasks = taskRepository.findAllByUserId(user.getId());
+        model.addAttribute("tasks",tasks);
         return "index";
     }
 
@@ -31,14 +48,33 @@ public class HomeController {
     }
 
     @PostMapping("form")
-    public String postForm(@ModelAttribute FullTaskDto fullTaskDto)
+    public String postForm(@ModelAttribute FullTaskDto fullTaskDto, Principal principal)
     {
         System.out.println("im in post form");
-        if(fullTaskDtoConverter.convertAndSave(fullTaskDto).equals("ok")){
+        if(fullTaskDtoConverter.convertAndSave(fullTaskDto,principal).equals("ok")){
             return "redirect:index";
         }
         else {
             return "redirect:form?error";
         }
+    }
+
+    @GetMapping("/login")
+    public String getLoginForm(Model model){
+        model.addAttribute("user",new User());
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("user",new User());
+        return "register";
+    }
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user){
+        user.setRole("ROLE_USER");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/login?created";
     }
 }
