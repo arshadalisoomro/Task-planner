c633e6f842705c3936e3659ba7eced60c206b2b8
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 15:57:30 +0200

    Add findAllByUserId

--- src/main/java/pl/leszekluksza/taskplanner/repository/TaskRepository.java
+++ src/main/java/pl/leszekluksza/taskplanner/repository/TaskRepository.java
@@ -3,8 +3,11 @@ package pl.leszekluksza.taskplanner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.leszekluksza.taskplanner.model.Task;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findById(Long id);
+
+    List<Task> findAllByUserId(Long userId);
 }
