89b1c831a919fa64bcf76c20cb0ccf2866df3bd0
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 15:57:14 +0200

    Add findByUsername

--- src/main/java/pl/leszekluksza/taskplanner/repository/UserRepository.java
+++ src/main/java/pl/leszekluksza/taskplanner/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package pl.leszekluksza.taskplanner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import pl.leszekluksza.taskplanner.model.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    UserDetails getUserByUsername(String username);
+
+    User findByUsername(String username);
 }
