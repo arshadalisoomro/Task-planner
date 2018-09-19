285f19b8e925834a1b9504517a6b2c4ee83466c8
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 14:06:39 +0200

    Add userdetails

--- src/main/java/pl/leszekluksza/taskplanner/model/User.java
+++ src/main/java/pl/leszekluksza/taskplanner/model/User.java
@@ -1,23 +1,35 @@
 package pl.leszekluksza.taskplanner.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.*;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 4)
+    @Column(unique = true)
     private String username;
+
+    @NotNull
     private String password;
+
+    private String role;
 
     @OneToMany(mappedBy = "user")
     private Set<Task> tasks = new HashSet<>();
 
     public User() {
+        this.role="ROLE_USER";
     }
 
     public User(String username, String password) {
@@ -29,8 +41,33 @@ public class User {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(getRole()));
     }
 
     public String getPassword() {
@@ -43,5 +80,25 @@ public class User {
 
     public Long getId() {
         return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
