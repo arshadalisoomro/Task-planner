76786b31b72fb57fd73261fdc7c3f6bef2a4f8c1
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-19 14:03:02 +0200

    Update SecurityConfig change inMemoryAuthentication to Authentication according to database

--- src/main/java/pl/leszekluksza/taskplanner/config/SecurityConfig.java
+++ src/main/java/pl/leszekluksza/taskplanner/config/SecurityConfig.java
@@ -1,27 +1,55 @@
 package pl.leszekluksza.taskplanner.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.leszekluksza.taskplanner.repository.UserRepository;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/css.**","index").permitAll()
-                    .antMatchers("/user/**").hasRole("USER")
-                    .and()
+                .antMatchers("/login*").anonymous()
+                .antMatchers("/user/**").hasRole("USER")
+                    .antMatchers("/form/**").hasRole("USER")
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/register").permitAll()
+                    .anyRequest().authenticated()
+                .and()
                     .formLogin()
-                    .loginPage("/login").failureUrl("/login-error");
+                   .loginPage("/login").failureUrl("/login?error")
+                    .and()
+                    .logout()
+                    .permitAll().and().csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.getUserByUsername(username);
+            }
+        }).passwordEncoder(passwordEncoder());
     }
 }
