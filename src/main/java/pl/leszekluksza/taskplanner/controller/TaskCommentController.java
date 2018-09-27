352680f709155275f67a4857f5dc6726a2eaa754
LeszekLuksza <leszekluksza90@gmail.com>
2018-09-27 15:53:03 +0200

    Add "AddNoteReceivePostRequest" method

--- src/main/java/pl/leszekluksza/taskplanner/controller/TaskCommentController.java
+++ src/main/java/pl/leszekluksza/taskplanner/controller/TaskCommentController.java
@@ -2,10 +2,16 @@ package pl.leszekluksza.taskplanner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.leszekluksza.taskplanner.converter.TaskCommentDtoConverter;
 import pl.leszekluksza.taskplanner.dao.TaskCommentDao;
+import pl.leszekluksza.taskplanner.dto.TaskCommentDto;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -14,8 +20,15 @@ public class TaskCommentController {
     @Autowired
     TaskCommentDao taskCommentDao;
 
-    @GetMapping("AddNote")
+    @Autowired
+    TaskCommentDtoConverter taskCommentDtoConverter;
+
+    @GetMapping("/AddNote")
     public String AddNote(@RequestParam String taskId, Principal principal){
         return taskCommentDao.checkTaskIdAndPrincipalAndReturnAddNotePageOrRedirect(taskId,principal);
+    }
+    @PostMapping("/AddNote")
+    public String AddNoteReceivePostRequest(@ModelAttribute @Valid TaskCommentDto taskCommentDto, Principal principal, BindingResult bindingResult){
+        return taskCommentDtoConverter.validateConvertSaveAndReturnIndex(taskCommentDto, principal, bindingResult);
     }
 }
